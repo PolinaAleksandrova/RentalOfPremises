7b207673897201b996b1b82f03637dfc844ff33a
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-12-01 22:32:17 +0200

    Initial commit, project setup.

--- RentalOfPremises/Startup.cs
+++ RentalOfPremises/Startup.cs
@@ -30,7 +30,10 @@ namespace RentalOfPremises
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
