2a8f690157d36d8bb8f3c9a9b56c2165663ce7a5
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-12-17 23:52:30 +0200

    @model Premises (AllPremises)

---
+++ RentalOfPremises/Data/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalOfPremises.Data.Models
+{
+    public class CartItem
+    {
+        public int ItemId { get; set; }
+        public Premises Premises { get; set; }
+        public int Price { get; set; }
+
+        public string CartId { get; set; }
+    }
+}
