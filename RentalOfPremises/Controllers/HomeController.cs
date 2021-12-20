a502bd19710634b5748eba1abe6578f309094e6f
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-12-20 23:43:19 +0200

    Home page fixed.

---
+++ RentalOfPremises/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentalOfPremises.Data.Services.Abstractions;
+using RentalOfPremises.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalOfPremises.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPremisesService _premisesService;
+
+        public HomeController(IPremisesService allPremises)
+        {
+            _premisesService = allPremises;
+        }
+        public ViewResult Index()
+        {
+            var homePremises = new HomeViewModel
+            {
+                BestOffer = _premisesService.GetBestOffer
+            };
+            return View(homePremises);
+        }
+    }
+}
