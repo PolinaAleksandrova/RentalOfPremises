28cb842a80c39b6c2cddcc922a6849d590db5f1f
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-12-25 16:50:33 +0200

    Categories of premises added.

--- RentalOfPremises/Startup.cs
+++ RentalOfPremises/Startup.cs
@@ -55,8 +55,12 @@ namespace RentalOfPremises
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession();
-            app.UseMvcWithDefaultRoute();
-
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "categoryFilter", template: "Premises/{action}/{category?}", defaults: new { Controller = "Premises", action = "GetList" });
+            });
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 Data.AppContext context = scope.ServiceProvider.GetRequiredService<Data.AppContext>();
