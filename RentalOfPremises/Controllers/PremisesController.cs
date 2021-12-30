e852af42d1e9e32a687ec55a83feb5bdc2726c79
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-12-30 11:57:47 +0200

    New categories added.

--- RentalOfPremises/Controllers/PremisesController.cs
+++ RentalOfPremises/Controllers/PremisesController.cs
@@ -44,7 +44,46 @@ namespace RentalOfPremises.Controllers
                     premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("RecordingStudio")).OrderBy(i => i.Id); ;
                     currCategory = "RecordingStudio";
                 }
-
+                else if (string.Equals("Shevchenkovsky", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Shevchenkovsky")).OrderBy(i => i.Id); ;
+                    currCategory = "Shevchenkovsky";
+                }
+                else if (string.Equals("Kyevsky", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Kyevsky")).OrderBy(i => i.Id); ;
+                    currCategory = "Kyevsky";
+                }
+                else if (string.Equals("Slobodskoy", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Slobodskoy")).OrderBy(i => i.Id); ;
+                    currCategory = "Slobodskoy";
+                }
+                else if (string.Equals("Osnovyansky", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Osnovyansky")).OrderBy(i => i.Id); ;
+                    currCategory = "Osnovyansky";
+                }
+                else if (string.Equals("Kholodnogorsky", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Kholodnogorsky")).OrderBy(i => i.Id); ;
+                    currCategory = "Kholodnogorsky";
+                }
+                else if (string.Equals("Moskovsky", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Moskovsky")).OrderBy(i => i.Id); ;
+                    currCategory = "Moskovsky";
+                }
+                else if (string.Equals("Novobavarsky", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Novobavarsky")).OrderBy(i => i.Id); ;
+                    currCategory = "Novobavarsky";
+                }
+                else if (string.Equals("Industrial", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    premises = _premisesService.Premises.Where(i => i.Category.Name.Equals("Industrial")).OrderBy(i => i.Id); ;
+                    currCategory = "Industrial";
+                }
             }
 
             var premisesObj = new PremisesListViewModel
